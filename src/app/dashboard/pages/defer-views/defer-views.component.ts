434b650b13b18f5a5034f4b1faf04c580bbcf057
Victor_Carmona <victor.carmona@grupoica.com>
2025-04-13 21:35:42 +0200

    Defer Blocks

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeavyLoadersSlowComponent],
   templateUrl: './defer-views.component.html',
   styleUrl: './defer-views.component.css'
 })
 export default class DeferViewsComponent {
 
+  
 }
