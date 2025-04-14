a65d0a0c84f40f0646498e1f31b05a74a0b18b7e
Victor_Carmona <victor.carmona@grupoica.com>
2025-04-14 07:49:04 +0200

    Defer Triggers

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from "../../../shared/title/title.component";
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [CommonModule, HeavyLoadersSlowComponent],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
   styleUrl: './defer-views.component.css'
 })
 export default class DeferViewsComponent {
 
-  
+
 }
